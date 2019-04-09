10646a6f6748446bc792b5aece054853638bf5d9
Serpean <sergi9p@gmail.com>
2019-04-09 13:34:42 +0200

    Minute 8:34 chapter 6

--- app.js
+++ app.js
@@ -22,11 +22,22 @@ app.use(
             date: String!
         }
 
+        type User {
+          _id: ID!
+          email: String!
+          password: String
+        }
+
         input EventInput {
             title: String!
             description: String!
             price: Float!
             date: String!
+        }
+
+        input UserInput {
+          email: String!
+          password: String!
         }
 
         type RootQuery {
